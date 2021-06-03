87a69f70cc4f2d8c6697950cb229170bf0aef571
mzw86020424 <mzw86020424@gmail.com>
2021-06-03 23:27:07 +0900

    Customize the Main Layout in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes, RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TriviaGameComponent } from './trivia-game/trivia-game.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'trivia', component: TriviaGameComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TriviaGameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
