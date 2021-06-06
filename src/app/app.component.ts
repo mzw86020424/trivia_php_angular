0c0aae0e5947854522e75ef12aee7697221b412c
mzw86020424 <mzw86020424@gmail.com>
2021-06-06 15:35:30 +0900

    Add Authentication with Okta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'trivia-web-client-angular';
+
+  isAuthenticated: boolean;
+
+  constructor(public oktaAuth: OktaAuthService) {
+      this.oktaAuth.$authenticationState.subscribe(
+          (isAuthenticated: boolean) => (this.isAuthenticated = isAuthenticated)
+      );
+  }
+
+  async ngOnInit() {
+      this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  }
+
+  login() {
+      this.oktaAuth.loginRedirect('/');
+  }
+
+  logout() {
+      this.oktaAuth.logout('/');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {Routes, RouterModule} from '@angular/router'
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { OktaAuthModule, OktaCallbackComponent } from '@okta/okta-angular';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,8 +11,15 @@ import { TriviaGameComponent } from './trivia-game/trivia-game.component';
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'trivia', component: TriviaGameComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: 'implicit/callback', component: OktaCallbackComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
+
+const oktaConfig = {
+  issuer: 'dev-55555894.okta.com',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oavytx3xnfZF44eb5d6'
+};
 
 @NgModule({
   declarations: [
@@ -18,10 +27,12 @@ const routes: Routes = [
     HomeComponent,
     TriviaGameComponent
   ],
-  imports: [
+imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
-  ],
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    OktaAuthModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
